4e5f0992767bcb89d83ee75f5b690fc931467adb
Alexander Ho <ho.alexander.g@gmail.com>
2023-05-07 11:43:13 +0800

    removed axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import Navigation from './components/Navigation';
 import NotFound from './components/NotFound';
-import { getBlogs } from './API';
 import { useGlobalCss } from './stitches.config';
 
-import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import '@fontsource/raleway/400.css';
 import '@fontsource/raleway/800.css';
 
 const App = () => {
-  const [blogs, setBlogs] = useState<IBlog[]>([]);
-
-  useEffect(() => {
-    fetchBlogs();
-  }, []);
-
   useGlobalCss();
-
-  const fetchBlogs = (): void => {
-    getBlogs()
-      .then(({ data: { blogs } }: IBlog[] | any) => setBlogs(blogs))
-      .catch((err: Error) => console.log(err));
-  };
 
   return (
     <>
