bab1091844b0435b91edd70d8f3d2e424ddbdae9
Alexander Ho <ho.alexander.g@gmail.com>
2023-05-12 16:19:37 +0800

    feat(react): added login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Login from './components/auth/Login';
 import Navigation from './components/Navigation';
 import AllBlogs from './components/blog/AllBlogs';
 import Blog from './components/blog/Blog';
@@ -5,6 +6,7 @@ import CreateBlog from './components/blog/CreateBlog';
 import NotFound from './components/NotFound';
 import { useGlobalCss } from './stitches.config';
 
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import '@fontsource/raleway/400.css';
 import '@fontsource/raleway/500.css';
@@ -14,10 +16,37 @@ import '@fontsource/raleway/800.css';
 const App = () => {
   useGlobalCss();
 
+  useEffect(() => {
+    const isUserAuth = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BASE_URL_SERVER}/isUserAuth`,
+          {
+            headers: {
+              'x-access-token': localStorage.getItem('token') as string,
+            },
+          }
+        );
+
+        const responseData = await response.json();
+        if (responseData.isloggedIn) {
+          console.log('success');
+        } else {
+          console.log('fail');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    isUserAuth();
+  }, []);
+
   return (
     <>
       <Navigation />
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/blogs">
           <Route index element={<AllBlogs />} />
           <Route path="create" element={<CreateBlog />} />
---
+++ client/src/components/auth/Login.tsx
@@ -0,0 +1,78 @@
+import stitches from '../../stitches.config';
+import { Form, Label, Input, Button } from '../stitches/form';
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const { styled } = stitches;
+
+const Main = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Title = styled('h2', {
+  marginBottom: '1rem',
+  fontSize: '2.5rem',
+});
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL_SERVER}/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+
+      const responseData = await response.json();
+      if (responseData.message === 'Success') {
+        localStorage.setItem('token', responseData.token);
+        navigate('/');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Main>
+      <Form onSubmit={handleLogin} size="fullscreen">
+        <Title>Log in</Title>
+        <Label htmlFor="form-username" mb0_5>
+          Username:
+        </Label>
+        <Input
+          id="form-username"
+          onChange={(e) => setUsername(e.target.value)}
+          mb1
+        />
+        <Label htmlFor="form-password" mb0_5>
+          Password:
+        </Label>
+        <Input
+          id="form-password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          mb1
+        />
+        <Button onClick={handleLogin}>Submit</Button>
+      </Form>
+    </Main>
+  );
+};
+
+export default Login;
