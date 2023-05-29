18520eda5e14d88948d3faf56702ea3def8b642e
Alexander Ho <ho.alexander.g@gmail.com>
2023-05-29 13:19:20 +0800

    feat(profile-page): added save handling

---
+++ client/src/components/Notification.tsx
@@ -0,0 +1,39 @@
+import stitches from '../stitches.config';
+import React from 'react';
+
+const { styled } = stitches;
+
+const NotificationContainer = styled('div', {
+  position: 'relative',
+  width: '100vw',
+  padding: '1.25rem 2rem',
+  color: 'White',
+  backgroundColor: 'Black',
+  fontSize: '$s',
+
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const CloseBtn = styled('button', {
+  border: 'none',
+  color: 'inherit',
+  backgroundColor: 'transparent',
+  fontSize: 'inherit',
+});
+
+interface Props {
+  message: React.ReactNode;
+  onClose: () => void;
+}
+
+const Notication = ({ message, onClose }: Props) => {
+  return (
+    <NotificationContainer>
+      {message} <CloseBtn onClick={onClose}>&#10005;</CloseBtn>
+    </NotificationContainer>
+  );
+};
+
+export default Notication;
