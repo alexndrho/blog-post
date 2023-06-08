1ab5c622d7a5132ef530c62a6cd0582d53e2e23a
Alexander Ho <ho.alexander.g@gmail.com>
2023-06-08 22:27:34 +0800

    feat(nav-bar): added profile icon and a dropdown

--- client/src/components/layout/Navigation.tsx
+++ client/src/components/layout/Navigation.tsx
@@ -1,6 +1,7 @@
 import { styled } from '../../stitches.config';
 import { A as AStyled } from '../common/elements';
 import { useAuth } from '../../context/useAuth';
+import { useUser } from '../../context/useUser';
 
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
@@ -40,6 +41,7 @@ const Menu = styled('div', {
   flex: '1',
   display: 'flex',
   justifyContent: 'space-between',
+  alignItems: 'center',
 });
 
 const Nav = styled('nav', {
@@ -87,16 +89,6 @@ const A = styled('a', {
   },
 });
 
-const AuthWrapperMobile = styled('div', {
-  flex: 1,
-  display: 'flex',
-  alignItems: 'center',
-
-  '@desktop': {
-    display: 'none',
-  },
-});
-
 const AuthWrapperDesktop = styled('div', {
   display: 'none',
 
@@ -134,9 +126,96 @@ const Bar = styled('span', {
   borderRadius: '1rem',
 });
 
+// profile icon
+const IconContainer = styled('div', {
+  position: 'relative',
+  cursor: 'pointer',
+});
+
+const Icon = styled('img', {
+  height: '$fontSizes$3',
+  aspectRatio: 1,
+  borderRadius: '50%',
+  objectFit: 'cover',
+  userSelect: 'none',
+
+  variants: {
+    blank: {
+      true: {
+        backgroundColor: 'Gray',
+      },
+    },
+  },
+});
+
+const ArrowContainer = styled('div', {
+  position: 'absolute',
+  bottom: 0,
+  right: 0,
+
+  backgroundColor: 'Black',
+  width: '$fontSizes$5',
+  aspectRatio: 1,
+  borderRadius: '50%',
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const Arrow = styled('span', {
+  fontSize: '0.65rem',
+  transform: 'rotate(90deg)',
+  transition: 'transform 0.25s',
+
+  '&::before': {
+    content: '\\276F',
+    color: 'White',
+  },
+
+  '&.active': {
+    transform: 'rotate(-90deg)',
+  },
+});
+
+const ProfileModal = styled('dialog', {
+  inset: 'calc(100% + 0.5rem) 0 auto auto',
+
+  borderRadius: '0.5rem',
+  border: 'none',
+  backgroundColor: 'Black',
+  overflow: 'hidden',
+});
+
+const ProfileModalLink = styled('a', {
+  display: 'block',
+  padding: '0.75rem 1rem',
+  textDecoration: 'none',
+  color: 'White',
+  fontSize: '$5',
+  fontWeight: '600',
+  cursor: 'pointer',
+  userSelect: 'none',
+  outline: 'none',
+
+  '&:hover': {
+    backgroundColor: 'Gray',
+  },
+});
+
+const tabletCss = {
+  '@desktop': {
+    display: 'none',
+  },
+};
+
 const Navigation = () => {
   const { isLoggedIn, setLoggedOut } = useAuth();
+  const { user, userIcon } = useUser();
   const linksRef = useRef<HTMLUListElement>(null);
+
+  const profileModalRef = useRef<HTMLDialogElement>(null);
+  const arrowRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
     window.addEventListener('resize', () => {
@@ -152,33 +231,29 @@ const Navigation = () => {
     setLoggedOut();
   };
 
+  const toggleProfileMenu = () => {
+    arrowRef.current?.classList.toggle('active');
+
+    profileModalRef.current?.open
+      ? profileModalRef.current?.close()
+      : profileModalRef.current?.show();
+  };
+
   //display nav links for mobile view
-  const displayNav = () => {
+  const toggleDisplayNav = () => {
     linksRef.current?.classList.toggle('active');
   };
 
   return (
     <Header>
       <Div>
-        <AuthWrapperMobile>
-          {isLoggedIn ? (
-            <AStyled onClick={logOut} size="btn_sm" color="outline">
-              Log out
-            </AStyled>
-          ) : (
-            <AStyled size="btn_sm" color="outline" as={Link} to="/signup">
-              Sign up
-            </AStyled>
-          )}
-        </AuthWrapperMobile>
-
         <TitleWrapper as={Link} to="/">
           <Title>BLOG</Title>
         </TitleWrapper>
 
         <Menu>
           <HambgMenuWrapper>
-            <HambgMenu onClick={displayNav}>
+            <HambgMenu onClick={toggleDisplayNav}>
               <Bar />
               <Bar />
               <Bar />
@@ -186,19 +261,86 @@ const Navigation = () => {
           </HambgMenuWrapper>
 
           <Nav ref={linksRef}>
-            <A as={Link} to="/" onClick={displayNav}>
+            {isLoggedIn && user?.username && (
+              <A
+                as={Link}
+                to={`/${user.username}`}
+                onClick={toggleDisplayNav}
+                css={tabletCss}
+              >
+                Profile
+              </A>
+            )}
+
+            <A as={Link} to="/" onClick={toggleDisplayNav}>
               Home
             </A>
-            <A as={Link} to="/blogs/create" onClick={displayNav}>
+            <A as={Link} to="/blogs/create" onClick={toggleDisplayNav}>
               Create blog
             </A>
+
+            {isLoggedIn ? (
+              <>
+                <A
+                  as={Link}
+                  to="/settings/profile"
+                  onClick={toggleDisplayNav}
+                  css={tabletCss}
+                >
+                  Settings
+                </A>
+
+                <A
+                  as={Link}
+                  to="/"
+                  onClick={() => {
+                    logOut();
+                    toggleDisplayNav();
+                  }}
+                  css={tabletCss}
+                >
+                  Log out
+                </A>
+              </>
+            ) : (
+              <>
+                <A
+                  as={Link}
+                  to="/signup"
+                  onClick={toggleDisplayNav}
+                  css={tabletCss}
+                >
+                  Sign up
+                </A>
+              </>
+            )}
           </Nav>
 
           <AuthWrapperDesktop>
             {isLoggedIn ? (
-              <AStyled onClick={logOut} size="btn_md" color="outline">
-                Log out
-              </AStyled>
+              <IconContainer onClick={toggleProfileMenu}>
+                {userIcon ? (
+                  <Icon src={userIcon} alt="profile-icon" />
+                ) : (
+                  <Icon blank as="div" />
+                )}
+
+                <ArrowContainer>
+                  <Arrow ref={arrowRef} />
+                </ArrowContainer>
+
+                <ProfileModal ref={profileModalRef}>
+                  <ProfileModalLink as={Link} to={`/${user?.username}`}>
+                    Profile
+                  </ProfileModalLink>
+                  <ProfileModalLink as={Link} to={`/settings/profile`}>
+                    Settings
+                  </ProfileModalLink>
+                  <ProfileModalLink as={Link} to="/" onClick={logOut}>
+                    Log out
+                  </ProfileModalLink>
+                </ProfileModal>
+              </IconContainer>
             ) : (
               <>
                 <AStyled size="btn_md" color="none" mr1 as={Link} to="/login">
