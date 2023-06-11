16bf0d2578b4c6b5f6c70b30cc79e34150f238f2
Alexander Ho <ho.alexander.g@gmail.com>
2023-06-11 17:58:11 +0800

    added disable react devtools for production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider.tsx';
 import UserProvider from './context/UserProvider.tsx';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (import.meta.env.PROD) disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
